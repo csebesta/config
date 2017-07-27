d41ae197ecec605c4c80196aefc5806b588761c5
Chad Sebesta <csebesta@users.noreply.github.com>
2017-07-26 21:09:15 -0700

    Changes to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-# User home directory configuration script
+# Home directory configuration script
 # Written by
 
 # Install packages from packages.txt
 #for i in packages.txt
 #	apt-get install -y
+#	echo "... Installed succesfully"
+
+# Resolve naming conflicts by moving existing files to an archive directory
 
 # Install configuration files using stow
 stow bash
