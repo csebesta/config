1f124765c66089e9c13cb063f1c93b879e1a71f3
Chad Sebesta <csebesta@users.noreply.github.com>
2017-07-26 20:19:14 -0700

    Modified setup script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# User home directory configuration script
+# Written by
+
+# Install packages from packages.txt
+#for i in packages.txt
+#	apt-get install -y
+
+# Install configuration files using stow
+stow bash
