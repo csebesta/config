f6e1832ccd076f0f33ab06325732c941c8044da8
Chad Sebesta <csebesta@users.noreply.github.com>
2017-08-04 21:37:38 -0700

    New xmonad configuration

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,7 +1,17 @@
 import XMonad
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
 
-main = xmonad def
-	{ terminal = "rxvt-unicode"
-	, borderWidth = 3
-	--, ((modMask, xK_p), spawn "dmenu_run")
-	}
+-- https://wiki.haskell.org/Xmonad/Config_archive/John_Goerzen's_Configuration
+-- https://beginners-guide-to-xmonad.readthedocs.io/configure_xmonadhs.html
+main = do
+	xmonad $ defaultConfig
+		-- Simple configruation
+		{ terminal = "rxvt-unicode"
+		, borderWidth = 3
+		} `additionalKeys`
+		-- Additional keybindings
+		[ ((mod1Mask, xK_p), spawn "dmenu_run -l 10 -fn 'DejaVu Sans Mono-15'")
+		, ((mod1Mask, xK_grave), spawn "slock")
+		]
