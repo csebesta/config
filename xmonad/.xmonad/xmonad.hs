7fb1efbf0af8a1036bfbe969b0b164994c73fcdb
Chad Sebesta <csebesta@users.noreply.github.com>
2018-02-26 23:36:39 -0700

    Major changes to xmonad configuration

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,16 +1,32 @@
+import System.IO
 import XMonad
+import XMonad.Actions.NoBorders
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
-import System.IO
-
 import XMonad.Layout.NoBorders (smartBorders)
-import XMonad.Actions.NoBorders
+import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.Run (spawnPipe)
 
--- Status bar
+------------------------------------------------------------------------
+-- Status bar (Pretty Printing)
+-- https://github.com/dmxt/Solarized-xmonad-xmobar
 myBar = "xmobar"
+myPP = xmobarPP
+	{  ppTitle = xmobarColor "#657b83" "" . shorten 100
+    , ppCurrent = xmobarColor "#c0c0c0" "" . wrap "" ""
+	, ppSep = xmobarColor "#c0c0c0" "" " | "
+    , ppUrgent = xmobarColor "#ff69b4" ""
+	, ppLayout = const "" -- Disables layout info
+	}
 
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+
+------------------------------------------------------------------------
+-- Colors and borders
+myNormalBorderColor = "#002b36"
+myFocusedBorderColor = "#657b83"
+
+------------------------------------------------------------------------
 -- Keybindings
 myKeys =
 
@@ -20,22 +36,26 @@ myKeys =
     \ -sb '#073642' \
     \ -nb '#002b36'" )
     , (( mod1Mask, xK_e ), spawn "nautilus --no-desktop" )
-    , (( mod1Mask, xK_b ), withFocused toggleBorder ) -- Toggle borders
     , (( mod1Mask, 0x2d ), spawn "amixer -q sset Master 5%-" ) -- Lower volume
     , (( mod1Mask, 0x3d ), spawn "amixer -q sset Master 5%+" ) -- Raise volume
     , (( mod1Mask, xK_BackSpace ), spawn "amixer -D pulse set Master toggle" ) -- Mute and unmute
     , (( mod1Mask .|. controlMask, xK_l ), spawn "slock" ) -- Lock screen
     ]
--- https://wiki.haskell.org/Xmonad/Config_archive/John_Goerzen's_Configuration
+
+------------------------------------------------------------------------
+-- Main function
 -- https://beginners-guide-to-xmonad.readthedocs.io/configure_xmonadhs.html
 -- https://unix.stackexchange.com/questions/336701/xmonad-defaults-depreciation-what-is-the-future-proof-configuration
---main = xmonad $ def
-main = do
-xmproc <- spawnPipe "xmobar"
-xmonad $ def
+-- https://wiki.haskell.org/Xmonad/Config_archive/John_Goerzen's_Configuration
+main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
 
+------------------------------------------------------------------------
+-- Main configuration
+myConfig = def
     { manageHook = manageDocks <+> manageHook def
     , layoutHook = smartBorders $ avoidStruts $ layoutHook def
 	, terminal = "rxvt-unicode"
-    , borderWidth = 3
+    , borderWidth = 1
+    , normalBorderColor = myNormalBorderColor
+    , focusedBorderColor = myFocusedBorderColor
     } `additionalKeys` myKeys
